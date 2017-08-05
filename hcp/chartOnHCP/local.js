e0fbb66e071883106dca9c74840e7477eb3fcf0c
i042416 <cle.ee@163.com>
2017-08-05 21:59:56 +0800

    can download image now

--- hcp/chartOnHCP/local.js
+++ hcp/chartOnHCP/local.js
@@ -1,5 +1,18 @@
+// https://stackoverflow.com/questions/12740659/downloading-images-with-node-js
+
 var path = require('path'), express = require('express');
+var fs = require('fs'), request = require('request');
 var app = express();
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    console.log("body: " + res);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
 
 app.use('/jerry', express.static(path.join(__dirname, 'webapp')));
 
@@ -29,6 +42,11 @@ app.post('/c4c', function(req, res){
     console.log('you posted: First Name: ' + req.body.firstName + ', 
     	Last Name: ' + req.body.lastName);*/
 	// console.log(req);
+    download('https://raw.githubusercontent.com/i042416/KnowlegeRepository/master/ABAP/BO/reuse_backend_code.md', 'google.png', 
+        function(){
+            console.log('done');
+        }
+   );
 	res.send("Hello C4C, post is got");
 });
 
