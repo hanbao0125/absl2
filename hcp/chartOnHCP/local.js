893d10d8c237b82a35760c88c4fe745419d277b5
i042416 <cle.ee@163.com>
2017-08-06 10:25:33 +0800

    try to print out account id but failed

--- hcp/chartOnHCP/local.js
+++ hcp/chartOnHCP/local.js
@@ -36,63 +36,23 @@ app.get('/c4c', function(req, res){
 
 app.use(bodyParser.json()); // for parsing application/json
 app.post('/c4c', function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    console.log("Jerry: " + req.body);
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;
+    // console.log("onData: " + chunk);
+  });
+  req.on('end', function () {
+    console.log('body end: ' + body);
+    var reqObj = JSON.parse(body);
+    console.log("Account id: " + reqObj);
+  })
 
-    /*download('https://raw.githubusercontent.com/i042416/KnowlegeRepository/master/ABAP/BO/reuse_backend_code.md',  
-        function(){
-            console.log('callback fired');
-        }
-   );*/
+    res.setHeader('Content-Type', 'application/json');
+
 request('https://raw.githubusercontent.com/i042416/KnowlegeRepository/master/ABAP/BO/reuse_backend_code.md', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
-    /*
-var options = {
-  //host: "proxy.wdf.sap.corp",
-  //port: 8080,
-  path: 'www.baidu.com/index.html'
-};
-
-var req2 = https.get(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-  // Buffer the body entirely for processing as a whole.
-  var bodyChunks = [];
-  res.on('data', function(chunk) {
-    // You can process streamed parts here...
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-    var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
-    // ...and/or process the entire body here.
-  })
-});
-
-req2.on('error', function(e) {
-  console.log('ERROR: ' + e.message);
-});
-*/
-// var http = require("http");
-
-/*var options = {
-  host: "proxy.wdf.sap.corp",
-  port: 8080,
-  path: "https://raw.githubusercontent.com/i042416/KnowlegeRepository/master/ABAP/BO/reuse_backend_code.md",
-  headers: {
-    Host: "www.google.com"
-  }
-};
-http.get(options, function(res) {
-  console.log("response: " + res);
-  res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-    });
-});
-*/
 	res.send("Hello C4C, post is got");
 });
 
