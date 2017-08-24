55b743b8ec34240ed89346e3e8f4598fc4abb95f
i042416 <cle.ee@163.com>
2017-08-24 14:14:14 +0800

    add load generator

---
+++ practice/loadGenerator/generator.js
@@ -0,0 +1,17 @@
+/*function funcOne(input) { 
+  var request = require('request');
+  request.post(someUrl, {json: true, body: input}, function(err, res, body) {
+      if (!err && res.statusCode === 200) {
+          funcTwo(body, function(err, output) {
+              console.log(err, output);
+          });
+      }
+  });
+}
+
+function funcTwo(input, callback) {
+    // process input
+    callback(null, input);
+}*/
+
+console.log("hello world");
