c31039dbf19ec9603d0127ca8d59a844fee7b67a
i042416 <cle.ee@163.com>
2017-12-09 10:24:24 +0800

    next:// pass control to the next handler

--- wechat/app/server.js
+++ wechat/app/server.js
@@ -17,14 +17,14 @@ var express = require('express'),
     app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
   // 该方法用于在所有请求方法的路径中装入中间件函数。
-  
+
     app.all('*', function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
       res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
       res.header("X-Powered-By",' 3.2.1');
       res.header("Content-Type", "application/json;charset=utf-8");
-      next();
+      next(); // pass control to the next handler
     });
     routes(app);
 
