8f17cdb0d63fdf194d997d04c0112054e35b0e6e
i042416 <cle.ee@163.com>
2017-12-09 11:38:29 +0800

    we can currently treat module as a keyword in nodejs environment

---
+++ wechat/app/a.js
@@ -0,0 +1,9 @@
+// Jerry 2017-12-9 11:39AM we can currently treat module as a keyword in nodejs environment
+
+console.log('module.id: ', module.id);
+console.log('module.exports: ', module.exports);
+console.log('module.parent: ', module.parent);
+console.log('module.filename: ', module.filename);
+console.log('module.loaded: ', module.loaded);
+console.log('module.children: ', module.children);
+console.log('module.paths: ', module.paths);
