72b971374b15b8bb0d6de21665ce00fbcb218476
i042416 <cle.ee@163.com>
2018-05-02 17:37:29 +0800

    demo can work

--- ABAP/blockchain/blockchain.js
+++ ABAP/blockchain/blockchain.js
@@ -50,3 +50,19 @@ class Blockchain{
   }
 }
 
+console.log("Jerry");
+
+let savjeeCoin = new Blockchain();
+
+savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
+savjeeCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
+
+// 检查是否有效(将会返回true)
+console.log('Blockchain valid? ' + savjeeCoin.isChainValid());
+
+// 现在尝试操作变更数据
+savjeeCoin.chain[1].data = { amount: 100 };
+
+// 再次检查是否有效 (将会返回false)
+console.log("Blockchain valid? " + savjeeCoin.isChainValid());
+
