c91e4e85c09dc03e0d63f736b46b04988046132a
i042416 <cle.ee@163.com>
2018-05-13 20:13:22 +0800

    cannot work in nodejs either

--- practice/EventBubble/SCP.js
+++ practice/EventBubble/SCP.js
@@ -6,7 +6,7 @@ var document_service = function(value){
 
 // //不能写成export sex这样的方式，如果这样就相当于export "boy",
 // 外部文件就获取不到该文件的内部变量sex的值，因为没有对外输出变量接口,只是输出的字符串。
-export {scp, document_service}  
+export {scp,document_service}  
 
 /*也可以写成
 export var sex="boy";export var echo=function(value){
---
+++ practice/EventBubble/consumer.js
@@ -0,0 +1,5 @@
+import {scp,document_service} from "./scp.js"
+
+console.log(scp);
+
+document_service("Cloud Foundry"); 
